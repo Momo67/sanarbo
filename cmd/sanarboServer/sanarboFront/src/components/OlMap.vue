d899d4d37c2ceeae79f3e0741c7a174a8c354d54
sgca0250 <noemi.romano@lausanne.ch>
2023-06-15 18:04:36 +0200

    autopopulate form

--- cmd/sanarboServer/sanarboFront/src/components/OlMap.vue
+++ cmd/sanarboServer/sanarboFront/src/components/OlMap.vue
@@ -12,6 +12,10 @@ import OlProjection from 'ol/proj/Projection'
 import {register} from 'ol/proj/proj4';
 import 'ol/ol.css'
 import {useFetch} from "../composables/FetchData.js";
+import Tree from "./Tree.vue";
+import {Select} from "ol/interaction.js";
+
+
 
 // Fetch data
 const backendUrl = import.meta.env.VITE_BACKEND_API_URL;
@@ -24,10 +28,30 @@ const options = {
   headers: headers
 }
 
-let errorFetch = ref(false);
-let errorFetchMessage = ref('');
-let fetchIsLoading = ref(true);
+const errorFetch = ref(false);
+const errorFetchMessage = ref('');
+const fetchIsLoading = ref(true);
 
+const showForm = ref(false);
+const treeId = ref(null);
+const newTree = ref(true);
+
+
+const selectInteraction = new Select();
+selectInteraction.on('select', (event) => {
+      if (event.selected.length > 0) {
+        showForm.value = true;
+        const selectedFeature = event.selected[0];
+        treeId.value = selectedFeature.get('id');
+        newTree.value = false;
+
+      } else {
+        showForm.value = false;
+        newTree.value = true;
+      }
+
+}
+)
 
 onMounted(   async () => {
 
@@ -51,23 +75,31 @@ onMounted(   async () => {
 
 
   // wkt to OL features
-  const wktFormat = new WKT();
+const wktFormat = new WKT();
 
-  const features = data.value.map((d) =>
-    wktFormat.readFeature(d.geom, {
-      featureProjection: swissProjection,
-    }));
+const features = data.value.map((d) => {
+
+  let feature = wktFormat.readFeature(d.geom, {
+    featureProjection: swissProjection,
+  })
+
+  feature.set('id', d.id)
+
+  return feature
+});
+
+
 
 
 // Define vector layer
-  const vectorLayer = new VectorLayer({
+const vectorLayer = new VectorLayer({
     source: new VectorSource({
       features: features
     })
   });
 
 
-  const mapInstance = new Map({
+const map = new Map({
     view: new View({
       center: 	[2537850.0, 1152445.0],
       zoom: 12,
@@ -82,16 +114,36 @@ onMounted(   async () => {
     target: 'map',
   });
 
+  map.addInteraction(selectInteraction)
+
+
+
 });
 </script>
 
 
 <template>
-
   <div id="map">
     <div v-if="fetchIsLoading">Loading...</div>
     <div v-else-if="errorFetch">Error: {{ errorFetchMessage }}</div>
   </div>
+
+
+  <v-dialog v-model="showForm">
+    <v-card>
+      <v-card-title>
+        Formulaire
+      </v-card-title>
+
+      <v-card-text>
+        <Tree :new-tree="newTree" :tree-id="treeId"></Tree>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="secondary" @click="showForm = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 
--- cmd/sanarboServer/sanarboFront/src/components/Tree.vue
+++ cmd/sanarboServer/sanarboFront/src/components/Tree.vue
@@ -1,14 +1,87 @@
 <script setup>
+import {onMounted, reactive} from 'vue';
+import {useFetch} from "../composables/FetchData.js";
+
+
+const backendUrl = import.meta.env.VITE_BACKEND_API_URL;
+const urlTrees = backendUrl + "trees"
+
+const props = defineProps({
+  newTree: Boolean,
+  treeId: Number
+})
+
+
+const Tree = reactive({
+  create_time: '',
+  creator: '',
+  description: '',
+  id: '',
+  name:'',
+});
+
+onMounted(async () => {
+
+  if (!props.newTree) {
+    const token = localStorage.getItem('token');
+    const headers = {'Authorization': 'Bearer ' + token}
+    const options = {
+      headers: headers
+    }
+
+    const {data} = await useFetch(urlTrees + '/' + props.treeId, options)
+    Tree.create_time = data.value.create_time;
+    Tree.creator = data.value.creator;
+    Tree.description = data.value.description;
+    Tree.id = data.value.id;
+    Tree.name = data.value.name;
+  }
+})
+
+
+
+
+
+const submitForm = () => {
+  // Handle form submission logic here
+  console.log(Tree);
+};
+
+
 
 </script>
 
 
-
 <template>
+  <v-form @submit="submitForm">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field v-model="Tree.id" label="ID"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+              v-model="Tree.create_time"
+              label="Date de création"
+              type="datetime"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field v-model="Tree.creator" label="Créateur"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field v-model="Tree.description" label="Description"></v-text-field>
+        </v-col>
 
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field v-model="Tree.name" label="Nom"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-btn type="submit" color="primary">Sauvegarder</v-btn>
+    </v-container>
+  </v-form>
 </template>
-
-
-<style scoped>
-
-</style>
