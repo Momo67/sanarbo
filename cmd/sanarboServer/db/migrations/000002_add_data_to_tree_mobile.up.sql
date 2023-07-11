05e837aaa356dea19c8bcd607128044c704602c1
Maurice Pittet <maurice.pittet@gmail.com>
2023-07-11 13:28:29 +0200

    Added some more datas to tree_mobile table

--- cmd/sanarboServer/db/migrations/000002_add_data_to_tree_mobile.up.sql
+++ cmd/sanarboServer/db/migrations/000002_add_data_to_tree_mobile.up.sql
@@ -1,10 +1,26 @@
-INSERT INTO tree_mobile
-(name, description, is_active, create_time, creator, geom, tree_attributes) 
-VALUES ('MyNewTree', 'Test de création', 't', CURRENT_TIMESTAMP, 999, ST_GeomFromText('POINT(2538221 1152372)', 2056), '{"idvalidation":1, "etatsanitairerem":"Rien à signaler"}');
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes)
+VALUES ('Acer platanoïdes - secteur EST - Arbres sur avenue - secteur EST',NULL,64749,'t',NULL,NULL,NULL,true,NULL,'2009-05-13 14:22:00',14330,'2019-04-16 23:01:00',12171,ST_GeomFromText('POINT(2538818.21 1152252.92)', 2056),'{"idvalidation" : 1, "idtobechecked" : 4, "idnote" : 4, "circonference" : 39, "identourage" : 1, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 1, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 3, "etatsanitairerem" : "", "envracinairerem" : ""}');
 
-INSERT INTO tree_mobile
-(name, description, is_active, create_time, creator, geom, tree_attributes) 
-VALUES ('Mon bel arbre', 'création d''un nouvel arbre', 't', CURRENT_TIMESTAMP, 999, ST_GeomFromText('POINT(2538221 1152372)', 2056), '{"idvalidation":2, "enracinairerem":"En ordre"}');
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Acer platanoïdes - Arbres sur avenue - secteur EST',NULL,65510,'t',NULL,NULL,NULL,false,NULL,'2009-06-02 10:17:00',14330,'2020-09-03 10:03:00',9916,ST_GeomFromText('POINT(2539592.74 1153578.07)', 2056),'{"idvalidation" : 5, "idtobechecked" : 4, "idnote" : 4, "circonference" : 32, "identourage" : 2, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 1, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 1, "etatsanitairerem" : "Taille.", "envracinairerem" : ""}');
+
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Acer platanoïdes - Arbres sur avenue - secteur EST',NULL,64996,'t',NULL,NULL,NULL,false,NULL,'2009-05-19 10:31:00',14330,'2020-06-25 10:28:00',9916,ST_GeomFromText('POINT(2538578.56 1152403.26)', 2056),'{"idvalidation" : 6, "idtobechecked" : 4, "idnote" : 4, "circonference" : 44, "identourage" : 1, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 1, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 1, "etatsanitairerem" : "", "envracinairerem" : ""}');
+
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Aesculus hippocastanum - secteur EST - Arbres sur avenue - secteur EST','Abattu 2019',65164,'t',NULL,NULL,NULL,false,NULL,'2009-05-25 10:59:00',14330,'2020-01-08 12:56:00',12171,ST_GeomFromText('POINT(2539093.15 1151904.71)', 2056),'{"idvalidation" : 7, "idtobechecked" : 4, "idnote" : 4, "circonference" : 174, "identourage" : 2, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 2, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 3, "etatsanitairerem" : "Tomo ok 2019", "envracinairerem" : ""}');
+
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Aesculus carnea - Arbres sur avenue - secteur EST',NULL,65203,'t',NULL,NULL,NULL,true,NULL,'2009-05-26 09:38:00',14330,'2019-04-18 23:01:00',12171,ST_GeomFromText('POINT(2539000.62 1151910.28)', 2056),'{"idvalidation" : 8, "idtobechecked" : 1, "idnote" : 4, "circonference" : 132, "identourage" : 2, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 2, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 1, "etatsanitairerem" : "Tomo\nChampignon au pied", "envracinairerem" : ""}');
+
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Acer platanoïdes - secteur EST - Arbres sur avenue - secteur EST',NULL,64959,'t',NULL,NULL,NULL,true,NULL,'2009-05-18 16:54:00',14330,'2019-04-16 23:01:00',12171,ST_GeomFromText('POINT(2538811.45 1152240.53)', 2056),'{"idvalidation" : 9, "idtobechecked" : 4, "idnote" : 3, "circonference" : 137, "identourage" : 4, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 1, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 1, "etatsanitairerem" : "tronc An. bles. à surveiller", "envracinairerem" : ""}');
+
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Crataegus media (x) ''Paul''s Scarlet'' - Arbres sur avenue - secteur SUD',NULL,71927,'t',NULL,NULL,NULL,true,NULL,'2009-11-27 08:04:00',14414,'2019-02-22 23:01:00',12171,ST_GeomFromText('POINT(2537453.57 1152010.87)', 2056),'{"idvalidation" : 10, "idtobechecked" : 4, "idnote" : 3, "circonference" : 40, "identourage" : 2, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 1, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 1, "etatsanitairerem" : "", "envracinairerem" : ""}');
+
+INSERT INTO tree_mobile (name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator, create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes) 
+VALUES ('Acer platanoïdes - secteur OUEST - Arbres sur avenue - secteur OUEST','Non-remplacé, axe fort.',65661,'t',NULL,NULL,NULL,false,NULL,'2009-06-03 16:01:00',14330,'2018-11-15 12:52:00',11515,ST_GeomFromText('POINT(2536223.88 1153159.53)', 2056),'{"idvalidation" : 11, "idtobechecked" : 1, "idnote" : 3, "circonference" : 87, "identourage" : 1, "idchkentourage" : 2, "entouragerem" : "", "idrevsurface" : 4, "idchkrevsurface" : 2, "revsurfacerem" : "", "idetatsanitairepied" : 2, "idetatsanitairetronc" : 1, "idetatsanitairecouronne" : 2, "etatsanitairerem" : "Abattage. Branches sèches.\nCompensation reportée aux axes forts", "envracinairerem" : ""}');
 
 INSERT INTO tree_mobile
   (
@@ -55,5 +71,5 @@ VALUES
     "idetatsanitairecouronne":1,
     "etatsanitairerem":"Taille. Racines un peu rouillées... Ca mériterait un coup de pinceau...",
     "envracinairerem":""}'
-    )
+    );
     
