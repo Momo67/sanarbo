41b0f5c32606b01e945630dceb65fc2c974647e5
Maurice Pittet <maurice.pittet@gmail.com>
2023-08-21 17:17:26 +0200

    Some improvments to tracking control

--- cmd/sanarboServer/sanarboFront/src/components/TrackingControl.vue
+++ cmd/sanarboServer/sanarboFront/src/components/TrackingControl.vue
@@ -2,36 +2,42 @@
 import { ref, reactive, onMounted } from 'vue';
 import Geolocation from 'ol/Geolocation.js';
 
+
 const props = defineProps({
   trackingEnabled: {
     type: Boolean,
     required: true
   },
-  olView: {
+  projection: {
     type: Object,
     required: true
   }
 });
 
 const trackingEnabled = ref(props.trackingEnabled);
-const olView = reactive(props.olView);
+const projection = reactive(props.projection);
 
-let geolocation = null;
+const emit = defineEmits(['toggle-tracking', 'position-changed']);
+
+const geolocation = new Geolocation({
+  // enableHighAccuracy must be set to true to have the heading value.
+  trackingOptions: {
+    enableHighAccuracy: true,
+  },
+  projection: projection,
+});
+
+const trackingOnClick = () => {
+  trackingEnabled.value = !trackingEnabled.value;
+  geolocation.setTracking(trackingEnabled.value);
+  emit('toggle-tracking', trackingEnabled.value);
+}
+
 
 onMounted(() => {
-  geolocation = new Geolocation({
-    // enableHighAccuracy must be set to true to have the heading value.
-    trackingOptions: {
-      enableHighAccuracy: true,
-    },
-    projection: olView.getProjection(),
-  });
   geolocation.setTracking(trackingEnabled.value);
   geolocation.on('change:position', () => {
-    olView.animate({
-      center: geolocation.getPosition(),
-      //duration: 2000,
-    });
+    emit('position-changed', geolocation.getPosition());
   });
 });
 </script>
