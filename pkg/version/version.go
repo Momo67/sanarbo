d227199a1795a6debd28f39d0ab49c2443a7f353
Maurice Pittet <maurice.pittet@gmail.com>
2024-08-19 10:11:17 +0200

    New minor version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "sanarbo"
 	AppSnake   = "sanarbo"
-	VERSION    = "0.0.3"
+	VERSION    = "0.1.0"
 	REPOSITORY = "github.com/lao-tseu-is-alive/sanarbo"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
