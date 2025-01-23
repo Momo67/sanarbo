c24d22387cf079bee8cdf8607a52396c6fdea1a2
Maurice Pittet <maurice.pittet@gmail.com>
2025-01-23 14:52:41 +0100

    Version 1.1.7

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "sanarbo"
 	AppSnake   = "sanarbo"
-	VERSION    = "1.1.6"
+	VERSION    = "1.1.7"
 	REPOSITORY = "github.com/lao-tseu-is-alive/sanarbo"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
