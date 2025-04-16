85d2f2ba2b84403074bf8709afb685e4ae9765fc
Maurice Pittet <maurice.pittet@gmail.com>
2025-04-16 16:55:38 +0200

    Now default layer is orthophotos_ortho_lidar_2024

--- cmd/sanarboServer/sanarboFront/src/config.js
+++ cmd/sanarboServer/sanarboFront/src/config.js
@@ -2,8 +2,8 @@ import { Log } from './log/index';
 
 export const APP = 'sanarboFront';
 export const APP_TITLE = 'Goéland';
-export const VERSION = '0.1.6';
-export const BUILD_DATE = '2024-12-06';
+export const VERSION = '2.2.0';
+export const BUILD_DATE = '2025-04-16';
 // eslint-disable-next-line no-undef
 export const DEV = process.env.NODE_ENV === 'development';
 // eslint-disable-next-line no-restricted-globals
@@ -13,4 +13,4 @@ export const apiRestrictedUrl = `goapi/v1`;
 export const getLog = (ModuleName, verbosityDev, verbosityProd) => (
   (DEV) ? new Log(ModuleName, verbosityDev) : new Log(ModuleName, verbosityProd)
 );
-export const DEFAULT_BASE_LAYER = 'orthophotos_ortho_lidar_2016';
+export const DEFAULT_BASE_LAYER = 'orthophotos_ortho_lidar_2024';
