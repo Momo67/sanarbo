4e4d7cd307dc42cf8583c590a67f7574aeb3eaa6
Maurice Pittet <maurice.pittet@gmail.com>
2025-10-29 16:06:24 +0100

    New version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "sanarbo"
 	AppSnake   = "sanarbo"
-	VERSION    = "2.2.3"
+	VERSION    = "2.2.4"
 	REPOSITORY = "github.com/lao-tseu-is-alive/sanarbo"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
